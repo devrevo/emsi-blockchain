4f1915753394406ada00553d9fedeb6d97bacb69
devrevo <ayoub.mafkoud@gmail.com>
2021-04-07 19:49:50 +0000

    Task 0.SHA256

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -20,7 +20,8 @@
  * struct sig_s - EC Signature structure
  *
  * @sig: Signature buffer. The whole space may not be used
- * @len: Actual signature size. Can't exceed SIG_MAX_LEN, therefore stored on a byte
+ * @len: Actual signature size. Can't exceed SIG_MAX_LEN,
+ *therefore stored on a byte
  */
 typedef struct sig_s
 {
@@ -31,7 +32,8 @@ uint8_t len;
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
-uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
+uint8_t *ec_to_pub(EC_KEY const *key,
+		   uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
 EC_KEY *ec_load(char const *folder);
