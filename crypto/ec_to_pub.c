a28e389d0a81313517f8011a8a63ac090afaf408
devrevo <ayoub.mafkoud@gmail.com>
2021-04-07 20:31:43 +0000

    TASK 2.EC TO PUB

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -3,6 +3,12 @@
 
 #include "hblk_crypto.h"
 
+/**
+ * ec_to_pub - extracts public key from EC_KEY struct
+ * @key: pointer to EC_KEY struct
+ * @pub: address of buffer to populate with pub key
+ * Return: address of populated buffer or NULL
+ */
 
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
