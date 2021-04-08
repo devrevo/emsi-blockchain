3cb12f8856aa93524d1642d275f147627544a161
devrevo <ayoub.mafkoud@gmail.com>
2021-04-08 02:36:54 +0000

    Task 7.EC Verify

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -4,15 +4,26 @@
 
 #include "hblk_crypto.h"
 
-
-int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t const *sig) {
-  unsigned char cmsg[SHA256_DIGEST_LENGTH];
-  
-  if(!msg || !key || !sig || !EC_KEY_check_key(key))
-    return 0;
-  if(!SHA256(msg,msglen,cmsg))
-    return 0;
-  if(ECDSA_verify(EC_CURVE, cmsg, SHA256_DIGEST_LENGTH, sig->sig, sig->len, (EC_KEY *)key) != 1)
-    return 0;
-  return 1;
+/**
+ * ec_verify - verifies the signature of a given set of bytes,
+ * using a given EC_KEY public key
+ * @key: points to the EC_KEY structure containing the public key
+ * to be used to verify the signature
+ * @msg: points to the characters to be signed
+ * @msglen: length of msg
+ * @sig: points to the signature to be checked
+ * Return: 1 if the signature is valid, and 0 otherwise
+ */
+int ec_verify(EC_KEY const *key, uint8_t const *msg,
+	      size_t msglen, sig_t const *sig)
+{
+unsigned char cmsg[SHA256_DIGEST_LENGTH];
+if (!msg || !key || !sig || !EC_KEY_check_key(key))
+return (0);
+if (!SHA256(msg, msglen, cmsg))
+return (0);
+if (ECDSA_verify(EC_CURVE, cmsg, SHA256_DIGEST_LENGTH,
+		 sig->sig, sig->len, (EC_KEY *)key) != 1)
+return (0);
+return (1);
 }
