c7ab3f592dcae70ca2fe09b9f6e45d23762e1f05
devrevo <ayoub.mafkoud@gmail.com>
2021-04-08 02:12:22 +0000

    Task 6.EC Signature

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -16,9 +16,13 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 	size_t msglen, sig_t *sig)
 {
+unsigned char cmsg[SHA256_DIGEST_LENGTH];
 unsigned int len;
 if (!key || !msg || !EC_KEY_check_key(key))
 return (NULL);
+if (!SHA256(msg, msglen, cmsg))
+return (NULL);
+sig->len = ECDSA_size(key);
 len = sig->len;
 if (!sig->len || sig->len > SIG_MAX_LEN)
 return (NULL);
