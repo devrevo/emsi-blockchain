17a278324c176c5b1974be66d93caf0eed347e78
devrevo <ayoub.mafkoud@gmail.com>
2021-04-10 19:11:37 +0000

    Task 3.Blockchain destroy

--- blockchain/v0.1/blockchain_destroy.c
+++ blockchain/v0.1/blockchain_destroy.c
@@ -3,7 +3,8 @@
  * blockchain_destroy - deletes the blockchain
  * @blockchain: blockchain to be deleted
  */
-void blockchain_destroy(blockchain_t *blockchain){
+void blockchain_destroy(blockchain_t *blockchain)
+{
 if (!blockchain)
 return;
 llist_destroy(blockchain->chain, 1, (node_dtor_t)free);
