995c950a654b3a364ebb48eddb6aff6a0b53f002
devrevo <ayoub.mafkoud@gmail.com>
2021-04-10 19:16:58 +0000

    Task 4.Block Hash

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,7 +1,15 @@
 #include <openssl/sha.h>
 #include "blockchain.h"
 
-
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]){
-  return (SHA256((const unsigned char *)block->data.buffer,(size_t)block->data.len,hash_buf));
+/**
+ * block_hash - get the hash of a block
+ * @block: block to be hashed, only hash field will not be hashed
+ * @hash_buf: buffer for digest
+ * Return: hash digest or NULL if failed
+ */
+uint8_t *block_hash(block_t const *block,
+		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+return (SHA256((const unsigned char *)block->data.buffer,
+	       (size_t)block->data.len, hash_buf));
 }
