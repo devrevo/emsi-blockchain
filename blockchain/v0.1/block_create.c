afb52d982784168e29cf3571cbc7748bb4ccfae0
devrevo <ayoub.mafkoud@gmail.com>
2021-04-10 13:55:55 +0000

    Task 1.Block create

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -1,4 +1,4 @@
-#include "blockchain.h"
+B#include "blockchain.h"
 #include <time.h>
 /**
  * block_create - creates a new block
@@ -7,44 +7,22 @@
  * @data_len: length of @data
  * Return: a block or NULL if failed
  */
+
 block_t *block_create(block_t const *prev,
 		      int8_t const *data, uint32_t data_len)
 {
 block_t *block;
-block_info_t *block_info;
-block_data_t *block_data;
-time_t t;
-int i;
-uint8_t *hash;
-block = (block_t *)malloc(sizeof(block_t));
-block_data = (block_data_t *)malloc(sizeof(block_data_t));
-block_info = (block_info_t *)malloc(sizeof(block_info_t));
-hash = (uint8_t *)malloc(sizeof(uint8_t) * SHA256_DIGEST_LENGTH);
-block_info->index = (uint32_t)((int)(prev->info.index) + 1);
-block_info->difficulty = 0;
-t = time(0);
-block_info->timestamp = (uint64_t)&t;
-block_info->nonce = 0;
-memcpy(block_info->prev_hash, prev->hash, sizeof(prev->hash));
-if (data_len >= (uint32_t)BLOCKCHAIN_DATA_MAX)
-{
-for (i = 0; i < BLOCKCHAIN_DATA_MAX; i++)
-block_data->buffer[i] = data[i];
-block_data->len = BLOCKCHAIN_DATA_MAX;
-}
-if (data_len < (uint32_t)BLOCKCHAIN_DATA_MAX)
-{
-for (i = 0; i < (int)data_len; i++)
-block_data->buffer[i] = data[i];
-block_data->len = data_len;
-}
-for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
-hash[i] = 0;
-memcpy(&block->data, ((block_data_t *)block_data), sizeof(block_data_t));
-memcpy(&block->info, ((block_info_t *)block_info), sizeof(block_info_t));
-memcpy(&block->hash, ((uint8_t *)hash), sizeof(uint8_t));
-free(block_data);
-free(block_info);
-free(hash);
+uint32_t max_len = data_len > BLOCKCHAIN_DATA_MAX ?
+BLOCKCHAIN_DATA_MAX : data_len;
+if (!prev || !data)
+return (NULL);
+block = calloc(1, sizeof(*block));
+if (!block)
+return (NULL);
+memcpy(block->data.buffer, data, max_len);
+block->data.len = max_len;
+memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+block->info.index = prev->info.index + 1;
+block->info.timestamp = (uint64_t)time(NULL);
 return (block);
 }
