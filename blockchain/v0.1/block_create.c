67ffde9c6645d9a7647361ccdabd83369582fafb
devrevo <ayoub.mafkoud@gmail.com>
2021-04-10 17:15:50 +0000

    Task 1.Block create

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -1,4 +1,4 @@
-B#include "blockchain.h"
+#include "blockchain.h"
 #include <time.h>
 /**
  * block_create - creates a new block
@@ -7,22 +7,35 @@ B#include "blockchain.h"
  * @data_len: length of @data
  * Return: a block or NULL if failed
  */
-
 block_t *block_create(block_t const *prev,
 		      int8_t const *data, uint32_t data_len)
 {
 block_t *block;
+time_t t;
+int i;
 uint32_t max_len = data_len > BLOCKCHAIN_DATA_MAX ?
 BLOCKCHAIN_DATA_MAX : data_len;
-if (!prev || !data)
+if (prev == NULL)
 return (NULL);
-block = calloc(1, sizeof(*block));
-if (!block)
+if (data == NULL)
 return (NULL);
-memcpy(block->data.buffer, data, max_len);
+block = (block_t *)malloc(sizeof(block_t));
+block->info.index = (uint32_t)((int)(prev->info.index) + 1);
+block->info.difficulty = 0;
+t = time(0);
+block->info.timestamp = (uint64_t)&t;
+block->info.nonce = 0;
+for (i = 0; i < (int)max_len; i++)
+{
+block->data.buffer[i] = data[i];
+}
 block->data.len = max_len;
-memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
-block->info.index = prev->info.index + 1;
-block->info.timestamp = (uint64_t)time(NULL);
+printf("BlockHash is");
+for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
+{
+block->hash[i] = 0;
+block->info.prev_hash[i] = ((block_t *)prev)->hash[i];
+printf("%d", block->hash[i]);
+}
 return (block);
 }
