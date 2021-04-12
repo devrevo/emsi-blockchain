2f7e696bb668f232858cd4b27bdbffa494e107d1
devrevo <ayoub.mafkoud@gmail.com>
2021-04-12 17:51:02 +0000

    Task 6.Signature

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -16,20 +16,18 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 	size_t msglen, sig_t *sig)
 {
-uint32_t len = 0;
-if (key == NULL)
-return (NULL);
-if (msg == NULL)
-return (NULL);
-if (!EC_KEY_check_key(key))
-return (NULL);
-bzero(sig->sig, sizeof(sig->sig));
-if (!ECDSA_sign(0, msg, msglen, sig->sig,
-	       &len, (EC_KEY *)key))
-{
-sig->len = 0;
-return (NULL);
-}
-sig->len = (uint8_t)len;
-return ((uint8_t *)sig->sig);
+  uint32_t len = 0;
+
+  if (!key || !msg || !msglen)
+    return (NULL);
+
+  bzero(sig->sig, sizeof(sig->sig));
+  if (!ECDSA_sign(0, msg, msglen, sig->sig, &len, (EC_KEY *)key))
+    {
+      sig->len = 0;
+      return (NULL);
+    }
+
+  sig->len = (uint8_t)len;
+  return ((uint8_t *)sig->sig);
 }
